47bf0e1c243ee6d86342cd72c77fac4c97e94dbe
MeghnaHarit <43702872+MeghnaHarit@users.noreply.github.com>
2019-02-26 19:28:06 -0800

    Add files via upload

---
+++ UpdateRecords.java
@@ -0,0 +1,53 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Scanner;
+
+public class UpdateRecords{
+		// TODO Auto-generated method stub
+     Connection con;
+     PreparedStatement pstmt;
+     ResultSet rs;
+     int cnt = 0;
+     String sqlUpdate;
+     public void update()
+     {
+     try
+     {
+    	 Class.forName("com.mysql.cj.jdbc.Driver");     //forName is for registering the driver
+		  con = DriverManager.getConnection("jdbc:mysql://localhost:3307/mysqljdbc","root","admin");
+    	 sqlUpdate = "UPDATE candidates " + "Set first_name=?" + "where id=?";
+		    pstmt=con.prepareStatement(sqlUpdate);
+		     //prepare data for update
+		    Scanner sc = new Scanner(System.in);
+    		System.out.println("enter candidate id:");
+    int id = sc.nextInt();  
+    sc.nextLine();
+    System.out.println("enter candidate name:");
+    String stm  = sc.next();  
+    sc.nextLine();
+    pstmt.setString(1, stm);
+    pstmt.setInt(2,id);
+    
+    int rowChanged = pstmt.executeUpdate();
+    System.out.println("row affected"+rowChanged);
+    
+    stm ="jean";
+    id =2;
+    pstmt.setString(1,stm);
+    pstmt.setInt(2,id);
+    rowChanged = pstmt.executeUpdate();
+    System.out.println("row affected" +rowChanged);
+    con.close();
+ }
+ catch(Exception e)
+ {
+	 System.out.println(e);
+ }
+}
+public class UpdateCommand{
+	
+}
+
+}
---
+++ UpdateResults.java
@@ -0,0 +1,9 @@
+public class UpdateResults {
+
+	public static void main(String[] args) throws Exception {
+		UpdateRecords ur=new UpdateRecords();
+	
+		ur.update();
+
+	}
+}
